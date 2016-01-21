46c5e2dc66138183dd724856eb6ce9c3215b17b0
Nikita Chernykh <chernykhmain@gmail.com>
2016-01-21 15:49:30 -0500

    Initial Commit
    Grid added

---
+++ Assets/Scripts/GridGenerator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GridGenerator : MonoBehaviour {
+	public GameObject prefab;
+	public float gridX = 5f;
+	public float gridY = 5f;
+	public float spacing = 2f;
+	
+	void Start() {
+		for (int y = 0; y < gridY; y++) {
+			for (int x = 0; x < gridX; x++) {
+				Vector2 pos = new Vector2(x, y) * spacing;
+				Instantiate(prefab, pos, Quaternion.identity);
+			}
+		}
+	}
+}
