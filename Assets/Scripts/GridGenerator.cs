f3b2f29e59b7d232f6bdd880328a3b33ea6c2856
Nikita Chernykh <chernykhmain@gmail.com>
2016-01-25 12:33:15 -0500

    e

--- Assets/Scripts/GridGenerator.cs
+++ Assets/Scripts/GridGenerator.cs
@@ -5,15 +5,10 @@ using System.IO;
 
 public class GridGenerator : MonoBehaviour {
 	public GameObject prefab;
-<<<<<<< HEAD
-	public float gridX;
-	public float gridY;
-	public float spacing;
-=======
+
 	public float gridX = 5f;
 	public float gridY = 5f;
 	public float spacing = 2f;
->>>>>>> c8aef8e6b4b2a7d1415a3bcf4e976536cdd0b7f5
 	void Start() {
 
 		for (int y = 0; y < gridY; y++) {
@@ -21,12 +16,7 @@ public class GridGenerator : MonoBehaviour {
 				Vector2 pos = new Vector2(x, y) * spacing;
 				Instantiate(prefab, pos, Quaternion.identity);
 				RNDColorGenerator();
-<<<<<<< HEAD
 				Debug.Log(pos);
-
-
-=======
->>>>>>> c8aef8e6b4b2a7d1415a3bcf4e976536cdd0b7f5
 			}
 		}
 	}
